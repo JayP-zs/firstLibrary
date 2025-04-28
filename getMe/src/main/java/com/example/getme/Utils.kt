3bd53f85ffc470022af6f76b1bb9a34e35c5afee
Jaya Prakash <jayaprakash.kunchapu@zopsmart.com>
2025-04-28 21:22:35 +0530

    initial commit

---
+++ getMe/src/main/java/com/example/getme/Utils.kt
@@ -0,0 +1,11 @@
+package com.example.getme
+
+object Utils {
+    fun sayHello(): String {
+        return "Hello from MyLibrary 👋"
+    }
+
+    fun sayHelloTOme(name: String): String {
+        return name
+    }
+}
